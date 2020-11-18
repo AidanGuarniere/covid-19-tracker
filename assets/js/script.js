60de5b2dea18e70ef29b5b2566aa0c3c80ef7dfc
aidan <aidanguarniere@gmail.com>
2020-11-18 15:07:33 -0800

    added initial fetch and display of covid data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,37 @@
+var covidTotalUrl = 'https://disease.sh/v3/covid-19/all' 
+
+
+
 // js functions for homepage dropdown
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, 'options');
   });
+
+// fetch covid-19 information
+function fetchCovid() {
+fetch('https://disease.sh/v3/covid-19/all')
+  .then(function (response) {
+    if (!response.ok) { // Request failed, go to catch
+      throw Error(response.statusText); // throw will stop execution of the promise chain and jump to catch
+    }
+    return response.json()
+  })
+  .then(function (data) {
+    console.log(data);
+    
+    displayCovidTotal(data);
+    
+  })
+  .catch(function (error) {
+    alert(error);
+  });  
+}
+
+fetchCovid()
+
+function displayCovidTotal(data){
+  var totalEl = document.querySelector("#total-cases")
+  totalEl.textContent =  "Total Active Cases: (global): " + data.active 
+
+}
