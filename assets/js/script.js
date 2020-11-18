ce800ae356814b399547486de4c83d82b8fed656
aidan <aidanguarniere@gmail.com>
2020-11-18 15:59:38 -0800

    applied starter data to basic hardcoded elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,7 @@ fetch('https://disease.sh/v3/covid-19/all')
   .then(function (data) {
     console.log(data);
     
-    displayCovidTotal(data);
+    displayCovidStarter(data);
     
   })
   .catch(function (error) {
@@ -29,9 +29,39 @@ fetch('https://disease.sh/v3/covid-19/all')
 }
 
 fetchCovid()
+// display covid starter info
+function displayCovidStarter(data){
 
-function displayCovidTotal(data){
-  var totalEl = document.querySelector("#total-cases")
-  totalEl.textContent =  "Total Active Cases: (global): " + data.active 
+  let countriesEl = document.querySelector("#affected-countries")
+  countriesEl.textContent = "Number of Affected Countries: " + data.affectedCountries
+
+
+  let ActiveEl = document.querySelector("#total-cases")
+  ActiveEl.textContent =  "Total Active Cases: (global): " + data.active 
+
+  let ActivePerOneMilEl = document.querySelector("#total-per-one-mil")
+  ActivePerOneMilEl.textContent = "Total Active Cases per One Million People (global): " + data.activePerOneMillion
+
+  let DeathsEl = document.querySelector("#total-deaths")
+  DeathsEl.textContent = "Total Deaths (global) " + data.deaths
+
+  let DeathsPerOneMilEl= document.querySelector("#total-deaths-per-one-mil")
+  DeathsPerOneMilEl.textContent = "Total Deaths from Covid-19 per One Million People (global): " + data.deathsPerOneMillion
+
+  let RecoveredEl = document.querySelector("#total-recovered")
+  RecoveredEl.textContent = "Number of People Recovered from Covid 19 (global): " + data.recovered
+
+  let RecoveredPerOneMillionEl = document.querySelector("#total-recovered-per-one-mil")
+  RecoveredPerOneMillionEl.textContent = "Number of People Recovered from Covid 19:  " + data.recoveredPerOneMillion
+
+  let CasesTodayEl = document.querySelector("#total-cases-today")
+  CasesTodayEl.textContent = "Number of Cases Today: " + data.todayCases
+
+  let deathsTodayEL = document.querySelector("#total-deaths-today")
+  deathsTodayEL.textContent = "Number of Deaths Today: " + data.todayDeaths
+
+  let recoveriesTodayEL = document.querySelector("#total-recovered-today")
+  recoveriesTodayEL.textContent = "Number of Recoveries Today: " + data.todayRecovered
+
 
 }
