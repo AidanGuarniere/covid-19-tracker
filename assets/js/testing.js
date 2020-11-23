c10d32cd25fb78e4559ec7bf41cf7f72540fbfae
Vanessa G <vlopez_jv@hotmail.com>
2020-11-22 22:15:27 -0800

    updated html ids, added js code

--- assets/js/testing.js
+++ assets/js/testing.js
@@ -1,1 +1,49 @@
-hey
+
+// const form_container = document.querySelector(".Symptom4");
+// const form_container = document.querySelector(".Symptom5");
+// const form_container = document.querySelector(".symptom6");
+// const form_container = document.querySelector(".symptom7");
+// const form_container = document.querySelector(".symptom8");
+// const form_container = document.querySelector(".symptom9");
+// const form_container = document.querySelector(".symptom10");
+// const form_container = document.querySelector(".symptom11");
+
+
+const s_1 = document.querySelector(".Sym1");
+const s_2 = document.querySelector(".Sym2");
+const s_3 = document.querySelector(".Sym3");
+const s_4 = document.querySelector(".Sym4");
+const s_5 = document.querySelector(".Sym5");
+const s_6 = document.querySelector(".Sym6");
+const s_7 = document.querySelector(".Sym7");
+const s_8 = document.querySelector(".Sym8");
+const s_9 = document.querySelector(".Sym9");
+const s_10 = document.querySelector(".Sym10");
+const s_11 = document.querySelector(".Sym11");
+
+const fever = document.querySelector(".value");
+const breath = document.querySelector(".value");
+
+let form_container = [value =1];
+ 
+
+const survey= document.querySelector(".form_container");
+const end = document.querySelector(".end");
+
+s_1.addEventListener("click", function () {
+    console.log("clicked")
+})
+
+s_2.addEventListener("click", function () {
+    console.log("clicked")
+    display= value
+})
+
+// function add(s_1,s_2 ) {
+
+//     let res = (s_1 + s_2)
+//     Ifreturn res{
+// console.log(res)}
+// }
+
+
