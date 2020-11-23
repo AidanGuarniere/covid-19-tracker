a7e77d7529480b0a971b2996dd5498c919c3245b
Vanessa G <vlopez_jv@hotmail.com>
2020-11-23 00:34:57 -0800

    testing js code

--- assets/js/testing.js
+++ assets/js/testing.js
@@ -1,14 +1,4 @@
-
-// const form_container = document.querySelector(".Symptom4");
-// const form_container = document.querySelector(".Symptom5");
-// const form_container = document.querySelector(".symptom6");
-// const form_container = document.querySelector(".symptom7");
-// const form_container = document.querySelector(".symptom8");
-// const form_container = document.querySelector(".symptom9");
-// const form_container = document.querySelector(".symptom10");
-// const form_container = document.querySelector(".symptom11");
-
-
+//testing constants by div
 const s_1 = document.querySelector(".Sym1");
 const s_2 = document.querySelector(".Sym2");
 const s_3 = document.querySelector(".Sym3");
@@ -20,30 +10,115 @@ const s_8 = document.querySelector(".Sym8");
 const s_9 = document.querySelector(".Sym9");
 const s_10 = document.querySelector(".Sym10");
 const s_11 = document.querySelector(".Sym11");
+const submitBtn = document.querySelector(".btn waves-effect waves-lighton");
 
-const fever = document.querySelector(".value");
-const breath = document.querySelector(".value");
+// //testing constants by id adding value
+// const fever = document.querySelector(".value");
+// const breath = document.querySelector(".value");
 
+//value of container
 let form_container = [value =1];
  
-
-const survey= document.querySelector(".form_container");
-const end = document.querySelector(".end");
+//testing by constants added eventlisteners
 
 s_1.addEventListener("click", function () {
-    console.log("clicked")
+    let p =  value;
+    for (let i = 0; i < value.length; i++) {
+        if (i < 0)
+        p += 0 };
+        console.log(p);
 })
 
 s_2.addEventListener("click", function () {
-    console.log("clicked")
-    display= value
+    let p =  value;
+    for (let i = 0; i < value.length; i++) {
+        if (i < 0)
+        p += 0 };
+        console.log(p);
+    })
+
+s_3.addEventListener("click", function () {
+        let p =  value;
+        for (let i = 0; i < value.length; i++) {
+            if (i < 0)
+            p += 0 };
+            console.log(p);
+    })
+
+s_4.addEventListener("click", function () {
+    let p =  value;
+    for (let i = 0; i < value.length; i++) {
+        if (i < 0)
+        p += 0 };
+        console.log(p);
 })
 
-// function add(s_1,s_2 ) {
+s_5.addEventListener("click", function () {
+    let p =  value;
+    for (let i = 0; i < value.length; i++) {
+        if (i < 0)
+        p += 0 };
+        console.log(p);
+})
 
-//     let res = (s_1 + s_2)
-//     Ifreturn res{
-// console.log(res)}
-// }
+s_6.addEventListener("click", function () {
+    let p =  value;
+    for (let i = 0; i < value.length; i++) {
+        if (i < 0)
+        p += 0 };
+        console.log(p);
+})
 
+s_8.addEventListener("click", function () {
+    let p =  value;
+    for (let i = 0; i < value.length; i++) {
+        if (i < 0)
+        p += 0 };
+        console.log(p);
+})
 
+s_9.addEventListener("click", function () {
+    let p =  value;
+    for (let i = 0; i < value.length; i++) {
+        if (i < 0)
+        p += 0 };
+        console.log(p);
+})
+
+s_10.addEventListener("click", function () {
+    let p =  value;
+    for (let i = 0; i < value.length; i++) {
+        if (i < 0)
+        p += 0 };
+        console.log(p);
+})
+
+s_11.addEventListener("click", function () {
+
+    let p =  value;
+    for (let i = 0; i < value.length; i++) {
+        if (i < 0)
+        p += 0 };
+        console.log(p);
+})
+
+// // ends survey
+// const survey= document.querySelector(".form_container");
+// const end = document.querySelector(".end");
+
+//  submitBtn.addEventListener("click", function (sum) {
+    let submitBtn_array = [s_1, s_2, s_3, s_4, s_5, s_6, s_7, s_8, s_9, s_10, s_11];
+    {
+         for (var i = 0 ; i < submitBtn_array.length ; ++i)
+        {
+             function click(index) 
+            {
+                submitBtn_array[index].addEventListener
+                (
+                     "click", function() {
+
+                     }) ( i);
+                
+            }
+        } 
+    }
