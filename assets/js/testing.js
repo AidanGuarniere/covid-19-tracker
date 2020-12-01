f2fdf09814efb4e7779b5968490ce5c715ea421a
Vanessa G <vlopez_jv@hotmail.com>
2020-12-01 13:33:39 -0800

    debugged modal js, edited testing html, and modified css.

--- assets/js/testing.js
+++ assets/js/testing.js
@@ -2,13 +2,17 @@
 var modal1 = document.getElementById("myModal1");
 var modal2 = document.getElementById("myModal2");
 var modal3 = document.getElementById("myModal3");
-
+var exit = document.getElementById("exit3");
 
 // Get the button that opens the modal
 var btn = document.getElementById("action");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+
+
+let myModal= ".modal";
+  
 
 // When the user clicks on the button, open the modal
 let score= 0;
@@ -20,22 +24,22 @@ document.querySelector("#action").addEventListener('click',function(event) {
         const element = symps[i];
         if (element.checked){
             score++;
-        }
+        } 
     }
-    })
+})
 
-btn.onclick = function() {
+
+action.onclick = function() {
     let tally= score;
     console.log(tally);
-    if(tally>=6){
-   myModal1.style.display="block"; //greater than  is danger zone
+    if(tally>=6){ 
+   myModal1.style.display="block";//greater than  is danger zone
                   }
      else if (tally ==5){
         myModal2.style.display="block"; //"greater than equal to 5 is warning";
                   } else{
                     myModal3.style.display="block"; // everything else or less than 4 is "good/green"
-                }                    
-            
- 
+                }            
 }
+
 
